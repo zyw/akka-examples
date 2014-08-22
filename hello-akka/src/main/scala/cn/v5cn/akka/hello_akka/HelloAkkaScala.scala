460522c9d25674e7160103ab79ce0a91f294ae9a
ZYW <zyw090111@163.com>
2014-08-22 17:08:03 +0800

    HelloAkka 完成

--- hello-akka/src/main/scala/cn/v5cn/akka/hello_akka/HelloAkkaScala.scala
+++ hello-akka/src/main/scala/cn/v5cn/akka/hello_akka/HelloAkkaScala.scala
@@ -1,6 +1,12 @@
 package cn.v5cn.akka.hello_akka
 
 import akka.actor.Actor
+import akka.actor.ActorSystem
+import akka.actor.Props
+import akka.actor.Inbox
+import scala.actors.ActorRef
+import akka.actor.ActorRef
+import scala.concurrent.duration._
 
 case object Greet
 
@@ -18,5 +24,30 @@ class Greeter extends Actor{
 }
 
 object HelloAkkaScala extends App {
+	val system = ActorSystem("helloakka");
+	val greeter = system.actorOf(Props[Greeter],name="greeter")
+	
+	val inbox = Inbox.create(system)
+	
+	greeter.tell(WhoToGreet("akka"), ActorRef.noSender)
+	
+	inbox.send(greeter, Greet)
+	
+	val Greeting(message1) = inbox.receive(5.seconds)
+	println(s"Greeing: $message1")
+	
+	greeter.tell(WhoToGreet("typesafe"), ActorRef.noSender)
+	inbox.send(greeter, Greet)
+	val Greeting(message2) = inbox.receive(5.seconds)
+	println(s"Greeting: $message2")
+	
+	val greetPrinter = system.actorOf(Props[GreetPrinter])
+	
+	system.scheduler.schedule(0.seconds, 1.second,greeter,Greet)(system.dispatcher,greetPrinter)
+}
 
+class GreetPrinter extends Actor{
+  def receive = {
+    case Greeting(message) => println(message)
+  }
 }
